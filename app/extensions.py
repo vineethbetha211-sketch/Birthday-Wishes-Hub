f710819ca54a47f861f51910b749fbe64fe7daf6
vineethbetha211-sketch <vineethbetha211@gmail.com>
2025-12-07 14:43:23 +0000

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_wtf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
+
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
 login_manager.login_message_category = "warning"
